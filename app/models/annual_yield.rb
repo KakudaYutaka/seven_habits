52ef37e46f8ba5a708f4cd4f10ce794ee145bb54
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-12-02 21:50:54 +0900

    activehash修正

--- app/models/annual_yield.rb
+++ app/models/annual_yield.rb
@@ -1,4 +1,4 @@
-class YearsId < ActiveHash::Base
+class AnnualYield < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '1' },
@@ -10,7 +10,7 @@ class YearsId < ActiveHash::Base
     { id: 8, name: '7' },
     { id: 9, name: '8' },
     { id: 10, name: '9' },
-    { id: 11, name: '10' },
+    { id: 11, name: "10" },
     { id: 12, name: '11' },
     { id: 13, name: '12' },
     { id: 14, name: '13' },
--- app/models/seven.rb
+++ app/models/seven.rb
@@ -1,6 +1,5 @@
 class Seven < ApplicationRecord
-
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :annual_yield_id
-  belongs_to_active_hash :years_id
+  belongs_to_active_hash :annual_yield
+  belongs_to_active_hash :years
 end
