20108bd21a6a5587a51a896c44369d752973cc02
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-12-02 22:18:38 +0900

    バリデーション設定

--- app/models/seven.rb
+++ app/models/seven.rb
@@ -1,4 +1,16 @@
 class Seven < ApplicationRecord
+
+  with_options presence: true do
+    validates :principal, numericality: { only_integer: true, allow_blank: true, message: 'Half-width number' }
+    validates :deposit, numericality: { only_integer: true, allow_blank: true, message: 'Half-width number' }
+  end
+
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :annual_yield_id
+    validates :years_id
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :annual_yield
   belongs_to_active_hash :years
