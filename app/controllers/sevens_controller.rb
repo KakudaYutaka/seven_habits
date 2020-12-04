2a1ff640fc69935b778423a6f8e705ef4d1f801b
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-12-04 18:14:18 +0900

    グラフの変数定義&計算式修正

--- app/controllers/sevens_controller.rb
+++ app/controllers/sevens_controller.rb
@@ -18,6 +18,10 @@ class SevensController < ApplicationController
   end
 
   def show
+    # 配列の場合
+    #@line_chart = [['2014-04-01', 30], ['2014-04-02', 40], ['2014-04-03', 50]]
+    # ハッシュの場合
+    @column_chart = {'元金' => @seven.principal, "#{@seven.years_id}年間積立合計" => @deposit2,'元金合計' => @principal2, "#{@seven.years_id}年後" => @compound.floor}
   end
 
   def judgment
@@ -46,13 +50,14 @@ class SevensController < ApplicationController
     @every_year = @seven.deposit * 12 #年間積立額
     @compound = @seven.principal 
     @principal2 = @seven.principal
+    @deposit2 = @seven.deposit * 12 * @seven.years_id #毎月積立額*12ケ月*年数
 
-    @seven.years_id * 12.times do |i|
+    (12 * @seven.years_id).times do |i|
       @compound =(@compound + @seven.deposit) * (1 + @seven.annual_yield_id / 100.to_f / 12) #複利
       @principal2 += @seven.deposit #元金トータル
     end
 
-    @difference = (@compound.floor - @principal2.floor)
+    @difference = (@compound.floor - @principal2.floor) #複利結果から元金トータルの差額
 
   end
 
