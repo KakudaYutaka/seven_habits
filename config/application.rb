befa9e6b754ef4cbf0c4589a50d3b5298ff6e2f9
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-12-10 18:37:50 +0900

    エラーメッセージ表示時崩れ防止

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module SevenHabits
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # 日本語の言語設定
+    config.i18n.default_locale = :ja
+
+    #エラーメッセージ表示時崩れ防止
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
