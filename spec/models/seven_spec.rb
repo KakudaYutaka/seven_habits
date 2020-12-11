e613222f0487d9585fafd001d7fd57e88b3a252b
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-12-11 15:31:10 +0900

    テストコード雛形

--- spec/models/seven_spec.rb
+++ spec/models/seven_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Seven, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @seven =  FactoryBot.build(:seven)
+  end
+
+  describe '複利計算機' do
+    context '複利計算機がうまくいくとき' do
+      it 'principal, deposit, annual_yield_id, years_id全て存在すれば計算される' do
+        expect(@seven).to be_valid
+      end
+    end
+
+    
+  end
+
 end
