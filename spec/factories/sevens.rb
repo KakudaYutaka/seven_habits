5e47a535840b591c2a525a43437ed0ea1cc34905
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-12-15 17:08:35 +0900

    単体テストコード実装

--- spec/factories/sevens.rb
+++ spec/factories/sevens.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :seven do
-    principal {1000000}
-    deposit {30000}
-    annual_yield_id {10}
-    years_id {20}
+    principal { 1_000_000 }
+    deposit { 30_000 }
+    annual_yield_id { 10 }
+    years_id { 20 }
   end
 end
--- spec/models/seven_spec.rb
+++ spec/models/seven_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Seven, type: :model do
   before do
-    @seven =  FactoryBot.build(:seven)
+    @seven = FactoryBot.build(:seven)
   end
 
   describe '複利計算機' do
@@ -12,7 +12,47 @@ RSpec.describe Seven, type: :model do
       end
     end
 
-    
+    context '複利計算機ががうまくいかないとき' do
+      it 'principalが空だと計算出力されない' do
+        @seven.principal = ''
+        @seven.valid?
+        expect(@seven.errors.full_messages).to include('元金を入力してください')
+      end
+      it 'principalが半角数字以外だと計算出力されない' do
+        @seven.principal = '３０００'
+        @seven.valid?
+        expect(@seven.errors.full_messages).to include('元金は半角数字で入力してください')
+      end
+      it 'principalが10000001以上だと計算出力されない' do
+        @seven.principal = '10000001'
+        @seven.valid?
+        expect(@seven.errors.full_messages).to include('元金は10000000以下で入力してください')
+      end
+      it 'depositが空だと計算出力されない' do
+        @seven.deposit = ''
+        @seven.valid?
+        expect(@seven.errors.full_messages).to include('毎月積立額を入力してください')
+      end
+      it 'depositが半角数字以外だと計算出力されない' do
+        @seven.deposit = '３０００'
+        @seven.valid?
+        expect(@seven.errors.full_messages).to include('毎月積立額は半角数字で入力してください')
+      end
+      it 'depositが10000001以上だと計算出力されない' do
+        @seven.deposit = '10000001'
+        @seven.valid?
+        expect(@seven.errors.full_messages).to include('毎月積立額は1000000以下で入力してください')
+      end
+      it 'annual_yield_idが0だと計算出力されない' do
+        @seven.annual_yield_id = '0'
+        @seven.valid?
+        expect(@seven.errors.full_messages).to include('年利率を選んでください')
+      end
+      it 'years_idが0だと計算出力されない' do
+        @seven.years_id = '0'
+        @seven.valid?
+        expect(@seven.errors.full_messages).to include('積立年数を選んでください')
+      end
+    end
   end
-
 end
